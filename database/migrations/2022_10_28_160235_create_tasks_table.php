482d5e1b97024720e18c4c48a7eb841181a13943
kousukeofkousuke <t420047@m.chukyo-u.ac.jp>
2022-10-29 01:10:02 +0900

    migration

---
+++ database/migrations/2022_10_28_160235_create_tasks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title',50);
+            $table->text('memo')->nullable();
+            $table->dateTime('deadline')->nullable();
+            $table->integer('target')->nullable();
+            $table->integer('score')->nullable();
+            $table->integer('priority')->nullable();
+            $table->integer('state')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
---
+++ database/migrations/2022_10_28_160309_create_targets_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTargetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('targets', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('short',50);
+            $table->string('middle',50);
+            $table->string('long',50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('targets');
+    }
+}
